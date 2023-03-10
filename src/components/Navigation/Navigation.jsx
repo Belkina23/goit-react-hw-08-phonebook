ffd218eea5b77ba59e1dba88067d25e3c3d7a6d5
Belkina23 <23zakat@gmail.com>
2023-03-10 17:49:19 +0200

    мипа

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
-import { UserMenu } from 'components/UserMenu/UserMenu';
-import { AuthNav } from 'components/AuthNav/AuthNav';
 
 export const Navigation = () => {
   const {isLoggedIn} = useAuth();
