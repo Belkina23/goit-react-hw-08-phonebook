98fa3ce9af9751582ae000b5bf776faa084f0e5c
Belkina23 <23zakat@gmail.com>
2023-03-11 17:28:39 +0200

    аываыв

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,10 @@
-import ContactForm from 'components/ContactForm/ContactForm';
-import Filter from 'components/Filter/Filter';
-import ContactsList from 'components/ContactsList/ContactsList';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Filter from '../components/Filter/Filter';
+import ContactsList from '../components/ContactsList/ContactsList';
 
 import { selectIsLoading, selectError } from 'redux/contacts/selectors';
 import { useSelector } from 'react-redux';
-import { AppStyled } from 'components/App/App.styled';
+// import { AppStyled } from 'components/App/App.styled';
 
 export const ContactPage = () => {
 
@@ -13,13 +13,13 @@ export const ContactPage = () => {
     console.log('Heloo')
     return (
   <main>
-      <AppStyled>
+      {/* <AppStyled> */}
         <h1>Phonebook</h1>
         <ContactForm />
         <Filter />
         {isLoading && !error && `...Loading`}
         <ContactsList />
-      </AppStyled>
+      {/* </AppStyled> */}
       </main>
     );
   };
