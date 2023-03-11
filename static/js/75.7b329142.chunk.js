a903669fc9bdea5f2f123f3c8b4fb04a762275de
Belkina23 <108288187+Belkina23@users.noreply.github.com>
2023-03-11 14:34:50 +0000

    Deploying to gh-pages from @ Belkina23/goit-react-hw-08-phonebook@d6fea8aad3c690a8335eddcf4f1e1f7c18356e24 🚀

---
+++ static/js/75.7b329142.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{6075:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});var n=r(184);function o(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Welcome to your phonebook!"})})}}}]);
+//# sourceMappingURL=75.7b329142.chunk.js.map
